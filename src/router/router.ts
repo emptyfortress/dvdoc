acbfaeace589978be4cf94363158f6139900713a
Volkov Alexander <volkov.525@gmail.com>
2022-10-11 14:45:33 +0300

    Fix types errors

--- src/router/router.ts
+++ src/router/router.ts
@@ -24,12 +24,6 @@ export const router = createRouter({
 			component: () => import('@/components/Version.vue'),
 			meta: { title: 'Docsvision docs', bread: ['Web-client', 'Release notes'] },
 		},
-		{
-			path: '/test',
-			name: 'test',
-			component: () => import('@/components/Test.vue'),
-			meta: { title: 'Docsvision docs', bread: ['Web-client', 'Release notes'] },
-		},
 	],
 })
 
